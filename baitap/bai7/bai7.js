bd697463503c3a5122891845641e75c80c0a1562
hex2k6 <hexgaming26@gmail.com>
2025-03-03 15:57:13 +0700

    Update bai7.js

--- baitap/bai7/bai7.js
+++ baitap/bai7/bai7.js
@@ -2,8 +2,4 @@ let Number1 = Number(prompt("nhập số thứ nhất: "));
 let Number2 = Number(prompt("nhập số thứ hai: "));
 let Number3 = Number(prompt("nhập số thứ ba: "));
 let higest = Math.max(Number1,Number2,Number3);
-if(!isNaN(Number1) && !isNaN(Number2) && !isNaN(Number3)){
-    alert("số lớn nhất trong 3 số là: " + higest);
-} else {
-    alert("vui lòng nhập số hợp lệ!");
-}
+alert(!isNaN(Number1) && !isNaN(Number2) && !isNaN(Number3) ? "số lớn nhất trong 3 số là: " + higest : "vui lòng nhập số hợp lệ!");
